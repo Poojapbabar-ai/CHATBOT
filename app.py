2df1000d01a714fa4e843f0a71e1969b42be8ace
Poojapbabar-ai <babarpooja2002@gmail.com>
2026-03-17 07:22:13 +0530

    added app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+
+import os
+from dotenv import load_dotenv
+import groq
+
+load_dotenv()
+
+# Read your prompt file
+with open("prompt.txt", "r") as f:
+    my_prompt = f.read()
+
+client = groq.Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+user_question = "what are the thyroxine 75 mcg price"
+
+response = client.chat.completions.create(
+    model="llama-3.1-8b-instant",
+    max_tokens=1024,
+    messages=[
+    {"role": "system", "content": my_prompt},   
+    {"role": "user", "content": user_question}
+]
+)
+
+print("Bot:", response.choices[0].message.content)
